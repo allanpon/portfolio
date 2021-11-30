7c0a036f4043bb6b249a894e2e2f67f9a43abb62
Allan Ponchaut <allan_ponchaut@ens.univ-artois.fr>
2021-11-30 23:31:47 +0100

    Avancé sur la partie titre de métier dans l'accueil

--- assets/app.js
+++ assets/app.js
@@ -28,3 +28,35 @@ toggleButton.addEventListener('click', () => {
 /*
     HEADER - END
  */
+
+/*
+    INDEX - BEGIN
+ */
+
+var p = document.getElementById('typewriter');
+var n = 0;
+var str = 'Développeur Web Symfony.';
+var typeTimer = setInterval(function() {
+
+    n = n + 1;
+    p.innerHTML = str.slice(0, n);
+    if (n === str.length) {
+        clearInterval(typeTimer);
+        p.innerHTML = str;
+        n = 0;
+        setInterval(function() {
+
+            if (n === 0) {
+                p.innerHTML = str + "|"
+                n = 1;
+            } else {
+                p.innerHTML = str
+                n = 0;
+            }
+        }, 500);
+    }
+}, 60)
+
+/*
+    INDEX - END
+ */
